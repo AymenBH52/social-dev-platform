f5954d2a6951c2701a3dd136b3bb9e874aaeef0e
Ben Hmida Aymen <118215572+AymenBH52@users.noreply.github.com>
2024-10-08 09:19:43 +0100

    Swagger Configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   //Configuration Swagger
+   const config = new DocumentBuilder()
+   .setTitle('API social-dev') // Titre de votre API
+   .setDescription('Documentation de l\'API social-dev') // Description
+   .setVersion('1.0') // Version de l'API
+   .build();
+   
+   const document = SwaggerModule.createDocument(app, config);
+
+   SwaggerModule.setup('api', app, document);
 
  
 
